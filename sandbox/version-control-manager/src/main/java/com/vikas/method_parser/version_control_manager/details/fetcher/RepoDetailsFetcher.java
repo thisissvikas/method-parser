a3301e813c7366248b0f9e165e0f6aab259bdfec
Vikas Kumar <krvikas1011@gmail.com>
2020-04-02 23:44:31 +0530

    Added code for fetching repo JSOn

--- sandbox/version-control-manager/src/main/java/com/vikas/method_parser/version_control_manager/details/fetcher/RepoDetailsFetcher.java
+++ sandbox/version-control-manager/src/main/java/com/vikas/method_parser/version_control_manager/details/fetcher/RepoDetailsFetcher.java
@@ -2,12 +2,22 @@ package com.vikas.method_parser.version_control_manager.details.fetcher;
 
 import com.vikas.method_parser.version_control_manager.model.SourceDetails;
 import com.vikas.method_parser.version_control_manager.model.UserInput;
+import com.vikas.method_parser.version_control_manager.util.GITHandler;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import java.net.URISyntaxException;
 
 public class RepoDetailsFetcher implements SourceDetailsFetcher{
     public SourceDetails getSourceDetails(UserInput userInput) {
-        JSONObject repoDetails = new JSONObject();
-        return getSourceDetails(repoDetails);
+        SourceDetails repoDetails = new SourceDetails();
+        try {
+            repoDetails = getSourceDetails(GITHandler.getGITRepo(userInput));
+        } catch (URISyntaxException | ParseException e){
+            // Todo: Need to implement logging.
+            e.printStackTrace();
+        }
+        return repoDetails;
     }
 
     private SourceDetails getSourceDetails(JSONObject repoDetails) {
---
+++ sandbox/version-control-manager/src/main/java/com/vikas/method_parser/version_control_manager/util/GITHandler.java
@@ -0,0 +1,32 @@
+package com.vikas.method_parser.version_control_manager.util;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.vikas.method_parser.version_control_manager.model.UserInput;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.net.URISyntaxException;
+
+public class GITHandler {
+  public static final String GIT_API_URL = "https://api.github.com";
+
+  public static JSONObject getGITRepo(UserInput userInput) throws URISyntaxException, ParseException {
+    String url = GIT_API_URL + "/users/" + userInput.getUserName() + "/repos";
+    ClientResponse clientResponse = RESTCallHandler.makeGETCall(url);
+    return getSpecificRepo(clientResponse, userInput.getRepositoryName());
+  }
+
+  private static JSONObject getSpecificRepo(ClientResponse clientResponse, String repositoryName) throws ParseException {
+    String output = clientResponse.getEntity(String.class);
+    JSONArray allRepos =  (JSONArray) new JSONParser().parse(output);
+    for(Object repoObject : allRepos){
+      JSONObject repo = (JSONObject) repoObject;
+      if(repositoryName.equalsIgnoreCase((String) repo.get("name"))){
+        return repo;
+      }
+    }
+    return null;
+  }
+}
