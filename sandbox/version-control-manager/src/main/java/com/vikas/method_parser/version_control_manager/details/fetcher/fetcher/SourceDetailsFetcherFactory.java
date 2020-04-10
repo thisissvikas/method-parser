635e93f69a1883636cceead60bf63ef451894d4d
Vikas Kumar <krvikas1011@gmail.com>
2020-04-10 18:59:56 +0530

    Switched to org fetcher

--- sandbox/version-control-manager/src/main/java/com/vikas/method_parser/version_control_manager/details/fetcher/fetcher/SourceDetailsFetcherFactory.java
+++ sandbox/version-control-manager/src/main/java/com/vikas/method_parser/version_control_manager/details/fetcher/fetcher/SourceDetailsFetcherFactory.java
@@ -5,6 +5,6 @@ import com.vikas.method_parser.version_control_manager.details.fetcher.model.Use
 public class SourceDetailsFetcherFactory {
     public static SourceDetailsFetcher getSourceDetailsFetcher(UserInput userInput){
         // Todo: Correct instance return logic will need to be implemented.
-        return new RepoDetailsFetcher();
+        return new OrgDetailsFetcher();
     }
 }
