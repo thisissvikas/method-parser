280c412bf63af5e528fdfde3996bb67f23a9995d
vikas.kumar <krvikas1011@gmail.com>
2020-04-11 21:25:31 +0530

    Checkout Manager (#14)
    * Added basic structure
    
    * Added changes for git checkout manager

---
+++ sandbox/version-control-manager/src/main/java/com/vikas/method_parser/version_control_manager/checkout/manager/SVNCheckoutManager.java
@@ -0,0 +1,10 @@
+package com.vikas.method_parser.version_control_manager.checkout.manager;
+
+import java.util.List;
+
+public class SVNCheckoutManager implements CheckoutManager{
+    @Override
+    public void checkout(List<String> repoURLs, String checkoutPath) {
+
+    }
+}
